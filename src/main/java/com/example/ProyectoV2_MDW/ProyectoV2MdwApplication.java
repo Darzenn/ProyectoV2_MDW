4258ae9caa6c97f8e657d04620abf873cfa5f0cf
Sebastian Tafur <eliottxt650@gmail.com>
2025-10-22 00:43:59 -0500

    iniciando proyecto V2

---
+++ src/main/java/com/example/ProyectoV2_MDW/ProyectoV2MdwApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoV2_MDW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoV2MdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoV2MdwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoV2_MDW/ProyectoV2MdwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoV2_MDW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoV2MdwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
