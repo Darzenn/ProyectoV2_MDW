68e077b8de4b3b5fc4e05b71a8ec8f737006ccca
ltsyy18 <dayanaacho04@gmail.com>
2025-11-03 19:33:43 -0500

    arreglando

--- src/main/java/com/example/ProyectoV2_MDW/repository/UsuarioRepository.java
+++ src/main/java/com/example/ProyectoV2_MDW/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package com.example.ProyectoV2_MDW.repository;
 
 import com.example.ProyectoV2_MDW.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
